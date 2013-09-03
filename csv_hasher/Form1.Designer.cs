743de20faa9161ed1512adeb60624e8121003947
Robert Landers <landers.robert@gmail.com>
2013-09-03 14:22:11 -0400

    Updated information

--- csv_hasher/Form1.Designer.cs
+++ csv_hasher/Form1.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File to Hash";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"CSV Files|*.csv|All Files|*.*\"";
+            this.openFileDialog1.Title = "Select a CSV File to Hash";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(733, 309);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hash";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Columns To Hash";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(220, 264);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Columns To Export";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(259, 32);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox2.Size = new System.Drawing.Size(216, 264);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(575, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"CSV File|*.csv|All Files|*.*\"";
+            this.saveFileDialog1.Title = "Save Export As";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 371);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- csv_hasher/Form1.cs
+++ csv_hasher/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CsvHelper;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace csv_hasher
 {
@@ -15,6 +18,111 @@ namespace csv_hasher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Dictionary<int, string> headers;
+        Dictionary<int, string> hash;
+        Dictionary<int, string> export;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            headers = new Dictionary<int, string>();
+            hash = new Dictionary<int, string>();
+            export = new Dictionary<int, string>();
+
+            using (TextReader file = File.OpenText(openFileDialog1.FileName))
+            {
+                using (CsvParser parser = new CsvParser(file))
+                {
+                    using (CsvReader reader = new CsvReader(parser))
+                    {
+                        reader.Read();
+                        int count = 0;
+                        foreach (var header in reader.FieldHeaders)
+                        {
+                            listBox1.Items.Add(header);
+                            listBox2.Items.Add(header);
+                            headers.Add(count++, header);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            hash = new Dictionary<int, string>();
+            foreach (var selected in listBox1.SelectedIndices)
+            {
+                if (!listBox2.SelectedIndices.Contains((int)selected))
+                {
+                    listBox2.SelectedIndices.Add((int)selected);
+                    hash.Add((int)selected, headers[(int)selected]);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+
+            using (TextReader file = File.OpenText(openFileDialog1.FileName))
+            {
+                using (CsvParser parser = new CsvParser(file))
+                {
+                    using (CsvReader reader = new CsvReader(parser))
+                    {
+                        using (TextWriter newfile = File.CreateText(saveFileDialog1.FileName))
+                        {
+                            using (CsvWriter writer = new CsvWriter(newfile))
+                            {
+                                reader.Read();
+
+                                foreach (var header in export)
+                                {
+                                    writer.WriteField(header.Value);
+                                }
+
+                                while (reader.Read())
+                                {
+                                    writer.NextRecord();
+                                    foreach (var field in export)
+                                    {
+                                        var content = reader.GetField(field.Key);
+                                        if (hash.ContainsKey(field.Key))
+                                        {
+                                            byte[] buffer = Encoding.UTF8.GetBytes(content);
+                                            var sha = SHA1Managed.Create();
+                                            content = BitConverter.ToString(sha.ComputeHash(buffer)).Replace("-", "").ToLower();
+                                        }
+                                        writer.WriteField(content);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            export = new Dictionary<int, string>();
+
+            foreach (var selected in listBox2.SelectedIndices)
+            {
+                if (!export.ContainsKey((int)selected))
+                    export.Add((int)selected, headers[(int)selected]);
+            }
         }
     }
 }
